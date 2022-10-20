6f08a9e90eb6cfc716f0f464638033ebb0fb408d
vxfontes <nessa1vane@icloud.com>
2022-10-20 09:18:26 -0300

    fix: colocando nova tela inicial

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,33 +1,85 @@
 import React from 'react';
 import styles from './styles';
-import { Container, Typography } from '@material-ui/core';
-import ColorButtonBlue from '../../components/button/Blue';
+import { Box, Button, Card, CardActions, CardContent, AppBar, Grid, Toolbar, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-import ColorButtonRed from '../../components/button/Red';
 
 const Home = () => {
 
     return (
         <>
-            <Container className={styles().root}>
-                <Container className={styles().container}>
-                    <Link to='/jm-system/comissao' style={{ textDecoration: 'none' }}>
-                        <ColorButtonBlue className={styles().paper}>
-                            <Typography variant='h5'>Recibo de <strong>comissão</strong></Typography>
-                        </ColorButtonBlue>
-                    </Link>
-                    <Link to='/jm-system/empresa' style={{ textDecoration: 'none' }}>
-                        <ColorButtonRed className={styles().paper}>
-                            <Typography variant='h5'>Recibo compra para <strong>empresas</strong></Typography>
-                        </ColorButtonRed>
-                    </Link>
-                    <Link to='/jm-system/cliente' style={{ textDecoration: 'none' }}>
-                        <ColorButtonBlue className={styles().paper}>
-                            <Typography variant='h5'>Recibo compra para <br /><strong>pessoa física</strong></Typography>
-                        </ColorButtonBlue>
-                    </Link>
-                </Container>
-            </Container>
+            <Box className={styles().image}>
+            </Box>
+            <AppBar>
+                <Toolbar>
+                    JM Paletes
+                </Toolbar>
+            </AppBar>
+            <Grid className={styles().root} container direction="row" justifyContent="center">
+
+                <Grid className={styles().grid} item xs={12} sm={12} md={6} lg={4}>
+                    <Card className={styles().rooters}>
+                        <CardContent>
+                            <Typography variant="h5" component="h2">
+                                Comissão
+                            </Typography>
+                            <Typography className={styles().pos} color="textSecondary">
+                                Destinado a funcionários da empresa
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                Requer assinatura de funcionário
+                            </Typography>
+                        </CardContent>
+                        <CardActions className={styles().grid}>
+                            <Link to='/jm-system/comissao' style={{ textDecoration: 'none' }}>
+                                <Button style={{ display: 'flex' }} size="small">gerar um recibo de comissão</Button>
+                            </Link>
+                        </CardActions>
+                    </Card>
+                </Grid>
+
+                <Grid className={styles().grid} item xs={12} sm={12} md={6} lg={4}>
+                    <Card className={styles().rooters}>
+                        <CardContent>
+                            <Typography variant="h5" component="h2">
+                                Empresa
+                            </Typography>
+                            <Typography className={styles().pos} color="textSecondary">
+                                Destinado a empresas compradoras
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                Requer dados pessoais da empresa
+                            </Typography>
+                        </CardContent>
+                        <CardActions className={styles().grid}>
+                            <Link to='/jm-system/empresa' style={{ textDecoration: 'none' }}>
+                                <Button size="small">gerar um recibo de venda</Button>
+                            </Link>
+                        </CardActions>
+                    </Card>
+                </Grid>
+
+                <Grid className={styles().grid} item xs={12} sm={12} md={3} lg={4}>
+                    <Card className={styles().rooters}>
+                        <CardContent>
+                            <Typography variant="h5" component="h2">
+                                Cliente
+                            </Typography>
+                            <Typography className={styles().pos} color="textSecondary">
+                                Destinado a clientes físicos
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                Requer dados pessoais do cliente
+                            </Typography>
+                        </CardContent>
+                        <CardActions className={styles().grid}>
+                            <Link to='/jm-system/cliente' style={{ textDecoration: 'none' }}>
+                                <Button size="small">gerar um recibo de venda</Button>
+                            </Link>
+                        </CardActions>
+                    </Card>
+                </Grid>
+
+            </Grid>
         </>
     );
 }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,30 +1,87 @@
 import { makeStyles } from '@material-ui/core';
+import { BorderBottom } from '@material-ui/icons';
+import deitado from '../../image/paleteDeitado.JPG';
+import emPe from '../../image/paleteEmPe.JPG';
 
-const styles = makeStyles({
+const styles = makeStyles((theme) => ({
+
+    image: {
+        height: '100vh',
+        width: '100%',
+        position: 'absolute',
+        margin: 0,
+
+        backgroundImage: (`url(${deitado})`),
+        filter: "blur(5px)",
+        border: 'none',
+        boxShadow: 'none',
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+        backgroundSize: "cover",
+        [theme.breakpoints.down('sm')]: {
+            backgroundImage: (`url(${emPe})`),
+        }
+
+    },
 
     root: {
-        // flexGrow: 1,
-        display: 'flex',
+        filter: "blur(0)",
+        // marginTop: "2%",
+        // marginBottom: '5%',
+        // width: '80%',
+        // maxWidth: '80%',
+        // gap: '20px'
         textAlign: 'center',
-        flexDirection: 'row'
+        paddingTop: 80
     },
 
-    container: {
-        marginTop: "2%",
-        marginBottom:'5%',
-        width: '80%',
-        maxWidth: '80%',
-        gap: '20px'
+    btn: {
+        borderRight: "1px solid rgba(255, 255, 255, 0.2)",
+        borderBottom: "1px solid rgba(255, 255, 255, 0.2)",
     },
 
-    paper: {
-        height: 200,
-        width: '80%',
-        marginLeft: '20px',
-        marginRight: '100px',
-        marginBottom: '20px',
+    grid: {
+        display: 'flex', 
+        justifyContent: 'center',
+    },
+    
+    rooters: {
+        marginTop: 50,
+        minWidth: 310,
+        backgroundColor: 'rgba(255, 255, 255, 1)',
+        [theme.breakpoints.down('xs')]: {
+            maxWidth: 310,
+        },
+        [theme.breakpoints.only('sm')]: {
+            minWidth: 450,
+            maxWidth: 450,
+        },
+        [theme.breakpoints.up('md')]: {
+            minWidth: 310,
+            maxWidth: 310,
+        },
+        [theme.breakpoints.up('lg')]: {
+            minWidth: 390,
+            maxWidth: 390,
+            marginTop: 100,
+        },
+        [theme.breakpoints.up('xl')]: {
+            minWidth: 600,
+            maxWidth: 600,
+            marginTop: 100,
+        },
+    },
+    bullet: {
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
     },
 
-});
+}));
 
 export default styles;
