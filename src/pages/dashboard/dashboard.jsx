72f505c979892c65339837dae9be958382b24c39
vxfontes <nessa1vane@icloud.com>
2022-10-21 00:02:04 -0300

    fix: finalizando todas as contas e chamadas da db

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,15 +1,25 @@
-const MainDashboard = ({ teste, comissao, despesas, compra }) => {
-    // vendas mensais
-    // lista das ultimas compras e vendas
-    // lucro total recebido bruto
-    // lucro total liquido
-    // despesas
-    // quantidade de paletes vendidos
-    // quantidade de paletes comprados
+import { doc, setDoc, deleteDoc } from 'firebase/firestore'
+import { dataBaseApp } from '../../firebase';
 
+const MainDashboard = () => {
+
+    // ordenar e armazenar tudo referente a um mes
+
+    // mudar tudo do mes atual para o mes anterior
+
+    // semana atual
+    setDoc(doc(dataBaseApp, "months", "mainMonth"), {
+        name: "MUDANDO ESSA PORRA",
+        state: "CA",
+        country: "USA"
+    });
+
+    // loop deletando todos os arquivos do mes ????
+    deleteDoc(doc(dataBaseApp, "cities", "LA"))
 
     return (
         <>
+            
         </>
     );
 }
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,4 +1,4 @@
-import { collection, addDoc, getDocs } from 'firebase/firestore'
+import { collection, getDocs } from 'firebase/firestore'
 import { useEffect, useState } from 'react';
 import { dataBaseApp } from "../../firebase";
 import MainDashboard from './dashboard';
@@ -9,84 +9,133 @@ const Dashboard = () => {
     const compraPalete = collection(dataBaseApp, "compraPalete");
     const despesasCd = collection(dataBaseApp, "despesas");
 
+    let data, empty
+    let totalVendas = 0, totalCompras = 0, totalComissao = 0, totalDespesas = 0;
+    let paletesVenda = 0, paletesCompra = 0;
+    let saidasTotais = 0, lucroLiq = 0;
+
     const [vendas, setVendas] = useState({});
-    const [vendasTotal, setVendasTotal] = useState(0);
-    let teste
     const [comissao, setComissao] = useState([]);
     const [despesas, setDespesas] = useState([]);
     const [compra, setCompra] = useState([]);
 
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [initCalc, setInitCalc] = useState(false);
 
     function getVendas() {
         getDocs(vendasRecibos).then((response) => {
-            const data = (response.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+            data = (response.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
             setVendas(data)
-            data.map((venda) => {
-                console.log(venda)
-                setVendasTotal(vendasTotal + venda.total)
-            })
-            console.log(vendasTotal)
         }).catch((error) => {
-            alert("DEU RUIM");
+            alert("Não foi possível conectar-se com o banco de dados");
+            console.log(error.response);
+        })
+    }
+
+    function getComissao() {
+        getDocs(comissaoRecibos).then((response) => {
+            data = (response.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+            setComissao(data)
+        }).catch((error) => {
+            alert("Não foi possível conectar-se com o banco de dados");
             console.log(error);
         })
     }
 
-    
-    // useEffect(() => {
-    //     setLoading(false);
+    function getDespesas() {
+        getDocs(despesasCd).then((response) => {
+            data = (response.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+            setDespesas(data)
+        }).catch((error) => {
+            alert("Não foi possível conectar-se com o banco de dados");
+            console.log(error);
+        })
+    }
 
-    //     // const getVendas = async () => {
-        //     //     const data = await getDocs(vendasRecibos);
-    //     //     // setVendas(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    //     //     teste = (data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    //     //     console.log(data);
-    //     //     console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    //     //     // console.log(vendas);
-    //     //     console.log(teste);
-    //     //     teste.map((venda) => {
-        //     //         console.log(venda)
-        //     //         setVendasTotal(vendasTotal + venda.total)
-        //     //     })
-        //     //     console.log(vendasTotal)
-        //     // };
-        
-    //     const getComissao = async () => {
-    //         const data = await getDocs(comissaoRecibos);
-    //         setComissao(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    //     };
-
-    //     const getDespesas = async () => {
-    //         const data = await getDocs(despesasCd);
-    //         setDespesas(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    //     };
-
-    //     const getCompra = async () => {
-    //         const data = await getDocs(compraPalete);
-    //         setCompra(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    //     };
-
-    //     getVendas();
-    //     getComissao();
-    //     getDespesas();
-    //     getCompra();
-    //     // console.log(vendas)
-    //     console.log(vendasTotal, "TOTAL DE VENDAS")
-    //     setLoading(true)
-    // }, []);
-
+    function getCompra() {
+        getDocs(compraPalete).then((response) => {
+            data = (response.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+            setCompra(data)
+            empty = response.empty;
+        }).catch((error) => {
+            alert("Não foi possível conectar-se com o banco de dados");
+            console.log(error);
+        })
+    }
 
     useEffect(() => {
+        setInitCalc(false)
         getVendas()
-        console.log(vendas);
-        console.log(vendasTotal);
-    }, [])
+        getComissao();
+        getDespesas();
+        getCompra();
+        setTimeout(() => {
+            // carrega e verifica se a ultima função recebe um array vazio
+            // se sim - nao termina o loading e redireciona usuario
+            if (empty === false) {
+                setInitCalc(true);
+                setLoading(true);
+            } else {
+                window.alert('Erro ao receber dados, por favor cadastre algum item')
+                window.location.replace("/jm-system/");
+            }
+        }, 3000)
+    }, [empty]);
 
+
+    function consoles() {
+        console.log("total compra", totalCompras)
+        console.log("total venda", totalVendas)
+        console.log("total despesa", totalDespesas)
+        console.log("total comissao", totalComissao)
+        console.log(saidasTotais, lucroLiq, paletesVenda, paletesCompra);
+    }
+
+    function somaTotais() {
+        vendas.map((venda) => {
+            let dt = Number(parseFloat(venda.total))
+            totalVendas = (totalVendas + dt)
+            paletesVenda = paletesVenda + venda.quantidade
+        })
+        comissao.map((comissao) => {
+            let dt = Number(parseFloat(comissao.total))
+            totalComissao = (totalComissao + dt)
+        })
+        despesas.map((Despesas) => {
+            let dt = Number(parseFloat(Despesas.valor))
+            totalDespesas = (totalDespesas + dt)
+        })
+        compra.map((compra) => {
+            let dt = Number(parseFloat(compra.total))
+            totalCompras = (totalCompras + dt)
+            paletesCompra = paletesCompra + compra.quantidade
+        })
+    }
+
+    
+
+    // principal da pagina
+    if (initCalc === true) {
+        somaTotais()
+        saidasTotais = (totalComissao + totalCompras + totalDespesas);
+        lucroLiq = totalVendas - saidasTotais;
+        consoles()
+    } else {
+        console.log('carregando dados');
+    }
 
     return (
         <>
-
+            {
+                loading ? (
+                    <>
+                        <h1>foi</h1>
+                        {/* <MainDashboard /> */}
+                    </>
+                ) : (
+                    <h1>carregando...</h1>
+                )
+            }
         </>
     )
 }
