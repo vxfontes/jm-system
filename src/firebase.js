8061b87e5429a8253a6fea21f94497ca6e0f7da4
vxfontes <nessa1vane@icloud.com>
2022-10-30 22:58:44 -0300

    fix: protegendo as chaves do projeto

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDVCMohneNBE1c0rpUVPxLpGA8Vro3_7kI",
-    authDomain: "jm-system.firebaseapp.com",
-    databaseURL: "https://jm-system-default-rtdb.firebaseio.com",
-    projectId: "jm-system",
-    storageBucket: "jm-system.appspot.com",
-    messagingSenderId: "639254228750",
-    appId: "1:639254228750:web:7847e9ad9e0cc496b0315f",
-    measurementId: "G-90ZBV5W9Q7"
-};
+import { firebaseConfig } from '../src/utils/firebaseKey.js'
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
